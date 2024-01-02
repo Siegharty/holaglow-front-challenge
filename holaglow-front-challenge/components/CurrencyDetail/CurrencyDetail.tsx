db06ef0548ec07a1360e5edbc2ef6a1c19c1b5d5
Siegharty <kcabrera866@gmail.com>
2024-01-02 19:58:13 -0300

    Adds data mock for detailed information about the currency

--- holaglow-front-challenge/components/CurrencyDetail/CurrencyDetail.tsx
+++ holaglow-front-challenge/components/CurrencyDetail/CurrencyDetail.tsx
@@ -7,38 +7,34 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
-import dataOverview from "@/dataOverview";
+import dataCryptoDetail from "@/dataCryptoDetailed";
 
 interface CurrencyDetailProp {
-    currencyToCheck : string;
+  currencyToCheck: string;
 }
 
-function CurrencyDetail({currencyToCheck}: CurrencyDetailProp) {
-
-  const filteredCurrency = dataOverview.filter((currencyDetail) => currencyDetail.symbol.toLowerCase() === currencyToCheck.toLowerCase());
+function CurrencyDetail({ currencyToCheck }: CurrencyDetailProp) {
+  //const filteredCurrency = dataOverview.filter((currencyDetail) => currencyDetail.symbol.toLowerCase() === currencyToCheck.toLowerCase());
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">Symbol</TableCell>
+            <TableCell>Symbol</TableCell>
             <TableCell align="right">Price</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {filteredCurrency.map((currency) => (
-            <TableRow
-              key={currency.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {currency.name}
-              </TableCell>
-              <TableCell align="right">{currency.symbol}</TableCell>
-              <TableCell align="right">{currency.quote.USD.price}</TableCell>
-            </TableRow>
-          ))}
+          <TableRow
+            key={dataCryptoDetail.symbol}
+            sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+          >
+            <TableCell component="th" scope="row">
+              {dataCryptoDetail.symbol}
+            </TableCell>
+
+            <TableCell align="right">{dataCryptoDetail.buy}</TableCell>
+          </TableRow>
         </TableBody>
       </Table>
     </TableContainer>
---
+++ holaglow-front-challenge/dataCryptoDetailed.tsx
@@ -0,0 +1,19 @@
+const dataCryptoDetail = {
+  time: 1704232962007,
+  symbol: "BTC-EUR",
+  buy: "41244.55",
+  sell: "41475.03",
+  changeRate: "0.0421",
+  changePrice: "1675.52",
+  high: "42228.05",
+  low: "39359.58",
+  vol: "5.18854",
+  volValue: "214396.3203833",
+  last: "41438.2",
+  averagePrice: "41130.37382247",
+  takerFeeRate: "0.001",
+  makerFeeRate: "0.001",
+  takerCoefficient: "1",
+  makerCoefficient: "1",
+};
+export default dataCryptoDetail;
