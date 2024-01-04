b9c239473f3e2356222089521d03be8530bcff3e
Siegharty <kcabrera866@gmail.com>
2024-01-04 19:42:56 -0300

    Fix overview page

--- holaglow-front-challenge/app/overview/page.tsx
+++ holaglow-front-challenge/app/overview/page.tsx
@@ -1,23 +1,61 @@
-import React, { useEffect } from "react";
+"use client";
+import React from "react";
+import { Grid, Typography } from "@mui/material";
 import CurrenciesList from "@/components/CurrenciesList/CurrenciesList";
-import useAsync from "@/components/hooks/useAsync";
 import BestCryptoList from "@/api/BestCryptoList/BestCryptoList.api";
-import { Currency } from "@/types/Currency";
-import SpinnerLoading from "@/components/SpinnerLoading/SpinnerLoading";
+import MarketPrices from "@/api/MarketPrices/MarketPrices.api";
+import MarketList from "@/components/MarketList/MarketList";
 
-export default function OverviewPage() {
-  const { data, error, loading } = useAsync<Currency[]>(BestCryptoList);
+async function BestCrypto() {
+  const getData = await BestCryptoList();
+  return getData;
+  //const result = await getData.json();
+  //const limitedArray = result.slice(0, 20);
+}
+
+async function MarketPricesData() {
+  const dataPrices = await MarketPrices();
+  return dataPrices;
+}
+
+export default async function OverviewPage() {
+  const bestCrypto = BestCrypto();
+  const dataMarketPrices = MarketPricesData();
+
+  const [dataCrypto, dataPrices] = await Promise.all([
+    bestCrypto,
+    dataMarketPrices,
+  ]);
+
+  const filteredMarketPricesByRank = () => {
+    if (dataCrypto !== undefined || dataCrypto !== null) {
+      const dataCryptoSymbols = dataCrypto.map((crypto) =>
+        crypto.symbol.toLowerCase()
+      );
+      return dataPrices.rates.filter((price) =>
+        dataCryptoSymbols.includes(Object.keys(price)[0].toLowerCase())
+      );
+    }
+  };
 
   return (
     <div>
-      Overview
-      {loading ? (
-        <SpinnerLoading />
-      ) : error ? (
-        <>Hubo un error</>
-      ) : (
-        <CurrenciesList currencies={data} />
-      )}
+      <Grid container direction={"row"}>
+        <Grid item xs={1}>
+          <Typography variant="h6" component="h2">
+            Overview
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid container spacing={6}>
+        <Grid item xs={6}>
+          {dataCrypto && <CurrenciesList currencies={dataCrypto} />}
+        </Grid>
+
+        <Grid item xs={6}>
+          <MarketList prices={filteredMarketPricesByRank()} />
+        </Grid>
+      </Grid>
     </div>
   );
 }
