a0b215a68adec6307bc471e2c0ca93d4e20f4103
Siegharty <kcabrera866@gmail.com>
2024-01-05 02:29:02 -0300

    Add type for crypto detail

---
+++ holaglow-front-challenge/types/CryptoDetail.ts
@@ -0,0 +1,18 @@
+export interface CryptoDetail {
+  time: number;
+  symbol: string;
+  buy: string;
+  sell: string;
+  changeRate: string;
+  changePrice: string;
+  high: string;
+  low: string;
+  vol: string;
+  volValue: string;
+  last: string;
+  averagePrice: string;
+  takerFeeRate: string;
+  makerFeeRate: string;
+  takerCoefficient: string;
+  makerCoefficient: string;
+}
